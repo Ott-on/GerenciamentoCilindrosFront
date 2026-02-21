172d7117ba75ffe48de0db0f187dc457bdd9dcfb
Vitor-Moura48 <90641243+Vitor-Moura48@users.noreply.github.com>
2026-02-21 19:50:37 -0300

    ajuste

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,14 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/backend/:path*',
+        destination: 'http://98.94.22.123:8080/:path*',
+      },
+    ]
+  },
+}
